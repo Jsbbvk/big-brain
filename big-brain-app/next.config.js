f72c4ffca49d239d2707b0cffa23b43feaa7604f
Jsbbvk <jsbbvk@gmail.com>
2020-12-18 18:24:15 -0800

    update

--- big-brain-app/next.config.js
+++ big-brain-app/next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     env: {
-        SOCKET_PORT: "ec2-54-202-163-159.us-west-2.compute.amazonaws.com",
+        SOCKET_PORT:
+            "https://ec2-54-202-163-159.us-west-2.compute.amazonaws.com",
     },
 };
