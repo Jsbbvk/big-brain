8f1b0c960faa504613a26b93391d1d04cf9b2f03
Jsbbvk <jsbbvk@gmail.com>
2020-12-18 17:38:03 -0800

    update port

--- big-brain-app/next.config.js
+++ big-brain-app/next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  env: {
-    SOCKET_PORT: "http://10.0.0.16:8080",
-  },
+    env: {
+        SOCKET_PORT: "ec2-54-202-163-159.us-west-2.compute.amazonaws.com:8080",
+    },
 };
