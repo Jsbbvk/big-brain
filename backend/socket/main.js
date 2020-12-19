ab2cb0f99531c53624b417a94ea598ed9d40e449
Jsbbvk <jsbbvk@gmail.com>
2020-12-18 17:14:12 -0800

    update

--- backend/socket/main.js
+++ backend/socket/main.js
@@ -4,45 +4,49 @@ const { dropCollection: dropPlayerCollection } = require("../storage/player");
 const { dropCollection: dropRoomCollection } = require("../storage/room");
 
 const connect = (server) => {
-  io = require("socket.io")(server, {
-    cors: {
-      origin: "*",
-    },
-  });
-  init();
+    io = require("socket.io")(server, {
+        cors: {
+            origin: "*",
+        },
+    });
+    init();
 };
 
 const emit = (room, emission, data) => {
-  io.to(room).emit(emission, data);
+    io.to(room).emit(emission, data);
 };
 
 const socket_emit = (socket, room, emission, data) => {
-  socket.to(room).emit(emission, data);
+    socket.to(room).emit(emission, data);
 };
 
 const init = () => {
-  //TODO remove
-  dropPlayerCollection().then((res) => console.log("reset player collection"));
-  dropRoomCollection().then((res) => console.log("reset room collection"));
+    //TODO remove
+    dropPlayerCollection().then((res) =>
+        console.log("reset player collection")
+    );
+    dropRoomCollection().then((res) => console.log("reset room collection"));
 
-  io.on("connection", (socket) => {
-    //console.log("connected");
+    io.on("connection", (socket) => {
+        //console.log("connected");
 
-    require("./room-socket")(socket);
-    require("./player-socket")(socket);
-    require("./gameplay-socket")(socket);
+        require("./room-socket")(socket);
+        require("./player-socket")(socket);
+        require("./gameplay-socket")(socket);
 
-    socket.on("drop collections", () => {
-      dropPlayerCollection().then((res) =>
-        //console.log("reset player collection")
-      );
-      dropRoomCollection().then((res) => console.log("reset room collection"));
+        socket.on("drop collections", () => {
+            dropPlayerCollection();
+            // .then((res) =>
+            //   console.log("reset player collection")
+            // );
+            dropRoomCollection();
+            // .then((res) => console.log("reset room collection"));
+        });
+
+        socket.on("disconnect", () => {
+            // console.log("disconnected");
+        });
     });
-
-    socket.on("disconnect", () => {
-      // console.log("disconnected");
-    });
-  });
 };
 
 module.exports = { default: connect, emit, socket_emit };
